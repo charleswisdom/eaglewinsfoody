62dbe32ccb438100aa62d32148e7b6eae6db9355
charleswisdom <144679143+charleswisdom@users.noreply.github.com>
2023-12-04 01:13:59 -0800

    Add files via upload

---
+++ my first project/script.js
@@ -0,0 +1,12 @@
+
+    $(" .owl-carousel ") .owlCarousel({
+        autoplay: true,
+        autoplayhoverpause: true,
+        autoplaytimeout: 100,
+        items: 1,
+        nav: true,
+        loop: true,
+        lazyLoad: true,
+        
+    })
+
